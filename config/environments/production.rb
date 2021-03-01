6250dff0587bcd4145986b2fb110f3d0a0eae1c7
Christoph Eicke <christoph@geisterstunde.org>
2021-03-01 20:00:04 +0100

    Give production URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_controller.default_url_options = { host: '192.168.0.2' }
 end
