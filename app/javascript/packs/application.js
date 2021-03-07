1dd45ed5f76201c261fc9574fd4c7667f3740a4b
Christoph Eicke <christoph@geisterstunde.org>
2021-03-07 12:32:14 +0100

    Replace bulma yarn package with gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bulma'
 
 Rails.start()
 Turbolinks.start()
