2ca6f03044dbde9bc3a668d1662305680d039945
Christoph Eicke <christoph.eicke@xing.com>
2023-01-31 09:15:07 +0100

    Preparation for external sonos command

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
+lock "~> 3.17.1"
 
 set :application, "music-history-wall"
 set :repo_url, "git@github.com:ceicke/music-history-wall.git"
