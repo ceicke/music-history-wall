985f85f1b48fbbc0c1581e32cfec04487202ba88
Christoph Eicke <christoph.eicke@xing.com>
2023-02-01 13:15:59 +0100

    Some more data in the charts section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
 
   def charts
-    @albums = Album.where('play_count > 0').order(play_count: :desc)
+    @albums = Album.where('play_count > 0').order(play_count: :desc).limit(50)
+    sql = "select sum(play_count) as total_plays from albums"
+    records_array = ActiveRecord::Base.connection.execute(sql)
+    @total_plays = records_array.first
   end
 
 end
